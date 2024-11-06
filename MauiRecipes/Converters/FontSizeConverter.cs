55bce9a536ddb7327d89958e87a27515f77ea566
fauxtix <fauxtix.luix@hotmail.com>
2024-11-06 10:47:00 +0000

    FontSize converter for when recipe title has longer/shorter lenghts

---
+++ MauiRecipes/Converters/FontSizeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace MauiRecipes.Converters
+{
+    public class FontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Ensure the value is a string and check its length
+            if (value is string title && title.Length > 70)
+            {
+                return 13; // Font size for long titles
+            }
+            return 16; // Font size for short titles
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
